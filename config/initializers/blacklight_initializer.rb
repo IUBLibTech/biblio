e8de9f24790c355e419d001b6a7a308118734463
Daniel Pierce <dlpierce@indiana.edu>
2015-02-13 12:41:36 -0500

    Generated Rails app with Spotlight template.

---
+++ config/initializers/blacklight_initializer.rb
@@ -0,0 +1,7 @@
+# A secret token used to encrypt user_id's in the Bookmarks#export callback URL
+# functionality, for example in Refworks export of Bookmarks. In Rails 4, Blacklight
+# will use the application's secret key base instead.
+#
+
+# Blacklight.secret_key = '855b604eb2ccb12a9ceb5e88df9fb34f21aacf79c76f7872aa507e711a70a89db6317fa3780d30ed3e786001f2566d899d79ab4858d94ca737eb8d2ed5f04513'
+
