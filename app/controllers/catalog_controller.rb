ba0346bd8ce09434aadab0c99768f45ff0fc7d60
Daniel Pierce <dlpierce@indiana.edu>
2015-02-13 17:00:58 -0500

    Use catalog_controller.rb from spotlight's internal test app.

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -1,31 +1,18 @@
 # -*- encoding : utf-8 -*-
-#
-class CatalogController < ApplicationController  
+require 'blacklight/catalog'
 
-  helper Openseadragon::OpenseadragonHelper
+class CatalogController < ApplicationController
 
   include Blacklight::Catalog
+  helper Openseadragon::OpenseadragonHelper
 
   configure_blacklight do |config|
-          config.show.oembed_field = :oembed_url_ssm
-          config.show.partials.insert(1, :oembed)
-          config.view.gallery.partials = [:index_header, :index]
-          config.view.slideshow.partials = [:index]
-
-          config.show.tile_source_field = :content_metadata_image_iiif_info_ssm
-          config.show.partials.insert(1, :openseadragon)
-
     ## Default parameters to send to solr for all search-like requests. See also SolrHelper#solr_search_params
-    config.default_solr_params = { 
-      :qt => 'search',
-      :rows => 10 
+    config.default_solr_params = {
+        :qt   => 'search',
+        :rows => 10,
+        :fl   => '*'
     }
-    
-    # solr path which will be added to solr base url before the other solr params.
-    #config.solr_path = 'select' 
-    
-    # items to show per page, each number in the array represent another option to choose from.
-    #config.per_page = [10,20,50,100]
 
     ## Default parameters to send on single-document requests to Solr. These settings are the Blackligt defaults (see SolrHelper#solr_doc_params) or 
     ## parameters included in the Blacklight-jetty document requestHandler.
@@ -39,12 +26,17 @@ class CatalogController < ApplicationController
     #}
 
     # solr field configuration for search results/index views
-    config.index.title_field = 'title_display'
-    config.index.display_type_field = 'format'
+    config.index.title_field = 'full_title_tesim'
+    config.index.display_type_field = 'content_metadata_type_ssm'
+    config.index.thumbnail_field = Spotlight::Engine.config.thumbnail_field
 
-    # solr field configuration for document/show views
-    #config.show.title_field = 'title_display'
-    #config.show.display_type_field = 'format'
+    config.view.gallery.partials = [:index_header, :index]
+    config.view.slideshow.partials = [:index]
+
+    config.show.tile_source_field = :content_metadata_image_iiif_info_ssm
+    config.show.partials.insert(1, :openseadragon)
+    # Alternative view that is just a thumbnail
+    #config.show.partials.insert(1, :image)
 
     # solr fields that will be treated as facets by the blacklight application
     #   The ordering of the field names is the order of the display
@@ -65,22 +57,12 @@ class CatalogController < ApplicationController
     #
     # :show may be set to false if you don't want the facet to be drawn in the 
     # facet bar
-    config.add_facet_field 'format', :label => 'Format'
-    config.add_facet_field 'pub_date', :label => 'Publication Year', :single => true
-    config.add_facet_field 'subject_topic_facet', :label => 'Topic', :limit => 20 
-    config.add_facet_field 'language_facet', :label => 'Language', :limit => true 
-    config.add_facet_field 'lc_1letter_facet', :label => 'Call Number' 
-    config.add_facet_field 'subject_geo_facet', :label => 'Region' 
-    config.add_facet_field 'subject_era_facet', :label => 'Era'  
-
-    config.add_facet_field 'example_pivot_field', :label => 'Pivot Field', :pivot => ['format', 'language_facet']
-
-    config.add_facet_field 'example_query_facet_field', :label => 'Publish Date', :query => {
-       :years_5 => { :label => 'within 5 Years', :fq => "pub_date:[#{Time.now.year - 5 } TO *]" },
-       :years_10 => { :label => 'within 10 Years', :fq => "pub_date:[#{Time.now.year - 10 } TO *]" },
-       :years_25 => { :label => 'within 25 Years', :fq => "pub_date:[#{Time.now.year - 25 } TO *]" }
-    }
-
+    config.add_facet_field 'genre_ssim', :label => 'Genre', :limit => true
+    config.add_facet_field 'personal_name_ssm', :label => 'Personal Names', :limit => true
+    config.add_facet_field 'corporate_name_ssm', :label => 'Corporate Names', :limit => true
+    config.add_facet_field 'subject_geographic_ssim', :label => 'Geographic'
+    config.add_facet_field 'subject_temporal_ssim', :label => 'Era'
+    config.add_facet_field 'language_ssim', :label => 'Language'
 
     # Have BL send all facet field names to Solr, which has been the default
     # previously. Simply remove these lines if you'd rather use Solr request
@@ -89,104 +71,37 @@ class CatalogController < ApplicationController
 
     # solr fields to be displayed in the index (search results) view
     #   The ordering of the field names is the order of the display 
-    config.add_index_field 'title_display', :label => 'Title'
-    config.add_index_field 'title_vern_display', :label => 'Title'
-    config.add_index_field 'author_display', :label => 'Author'
-    config.add_index_field 'author_vern_display', :label => 'Author'
-    config.add_index_field 'format', :label => 'Format'
-    config.add_index_field 'language_facet', :label => 'Language'
-    config.add_index_field 'published_display', :label => 'Published'
-    config.add_index_field 'published_vern_display', :label => 'Published'
-    config.add_index_field 'lc_callnum_display', :label => 'Call number'
+    config.add_index_field 'language_ssm', :label => 'Language'
+    config.add_index_field 'abstract_tesim', :label => 'Abstract'
+    config.add_index_field 'note_mapuse_tesim', :label => 'Type'
+    config.add_index_field 'note_source_tesim', :label => 'Source'
+    config.add_index_field 'subject_geographic_tesim', :label => 'Geographic Subject'
+    config.add_index_field 'subject_temporal_tesim', :label => 'Temporal Subject'
+    config.add_index_field Spotlight::Engine.config.uploaded_description_field, :label => 'Description'
+    config.add_index_field Spotlight::Engine.config.uploaded_attribution_field, :label => 'Attribution'
+    config.add_index_field Spotlight::Engine.config.uploaded_date_field, :label => 'Date'
 
     # solr fields to be displayed in the show (single result) view
     #   The ordering of the field names is the order of the display 
-    config.add_show_field 'title_display', :label => 'Title'
-    config.add_show_field 'title_vern_display', :label => 'Title'
-    config.add_show_field 'subtitle_display', :label => 'Subtitle'
-    config.add_show_field 'subtitle_vern_display', :label => 'Subtitle'
-    config.add_show_field 'author_display', :label => 'Author'
-    config.add_show_field 'author_vern_display', :label => 'Author'
-    config.add_show_field 'format', :label => 'Format'
-    config.add_show_field 'url_fulltext_display', :label => 'URL'
-    config.add_show_field 'url_suppl_display', :label => 'More Information'
-    config.add_show_field 'language_facet', :label => 'Language'
-    config.add_show_field 'published_display', :label => 'Published'
-    config.add_show_field 'published_vern_display', :label => 'Published'
-    config.add_show_field 'lc_callnum_display', :label => 'Call number'
-    config.add_show_field 'isbn_t', :label => 'ISBN'
+    config.add_show_field 'note_phys_desc_tesim', :label => 'Note'
+    config.add_show_field 'note_source_tesim', :label => 'Source'
+    config.add_show_field 'note_desc_note_tesim', :label => 'Note'
+    config.add_show_field 'note_references_tesim', :label => 'References'
+    config.add_show_field 'note_provenance_tesim', :label => 'Provenance'
+    config.add_show_field 'note_page_num_tesim', :label => 'Page Number'
+    config.add_show_field 'subject_geographic_tesim', :label => 'Geographic Subject'
+    config.add_show_field 'subject_temporal_tesim', :label => 'Temporal Subject'
+    config.add_show_field 'personal_name_ssm', :label => 'Personal Names'
+    config.add_show_field 'corporate_name_ssm', :label => 'Corporate Names'
 
-    # "fielded" search configuration. Used by pulldown among other places.
-    # For supported keys in hash, see rdoc for Blacklight::SearchFields
-    #
-    # Search fields will inherit the :qt solr request handler from
-    # config[:default_solr_parameters], OR can specify a different one
-    # with a :qt key/value. Below examples inherit, except for subject
-    # that specifies the same :qt as default for our own internal
-    # testing purposes.
-    #
-    # The :key is what will be used to identify this BL search field internally,
-    # as well as in URLs -- so changing it after deployment may break bookmarked
-    # urls.  A display label will be automatically calculated from the :key,
-    # or can be specified manually to be different. 
-
-    # This one uses all the defaults set by the solr request handler. Which
-    # solr request handler? The one set in config[:default_solr_parameters][:qt],
-    # since we aren't specifying it otherwise. 
-    
-    config.add_search_field 'all_fields', :label => 'All Fields'
-    
-
-    # Now we see how to over-ride Solr request handler defaults, in this
-    # case for a BL "search field", which is really a dismax aggregate
-    # of Solr search fields. 
-    
-    config.add_search_field('title') do |field|
-      # solr_parameters hash are sent to Solr as ordinary url query params. 
-      field.solr_parameters = { :'spellcheck.dictionary' => 'title' }
-
-      # :solr_local_parameters will be sent using Solr LocalParams
-      # syntax, as eg {! qf=$title_qf }. This is neccesary to use
-      # Solr parameter de-referencing like $title_qf.
-      # See: http://wiki.apache.org/solr/LocalParams
-      field.solr_local_parameters = { 
-        :qf => '$title_qf',
-        :pf => '$title_pf'
-      }
-    end
-    
-    config.add_search_field('author') do |field|
-      field.solr_parameters = { :'spellcheck.dictionary' => 'author' }
-      field.solr_local_parameters = { 
-        :qf => '$author_qf',
-        :pf => '$author_pf'
-      }
-    end
-    
-    # Specifying a :qt only to show it's possible, and so our internal automated
-    # tests can test it. In this case it's the same as 
-    # config[:default_solr_parameters][:qt], so isn't actually neccesary. 
-    config.add_search_field('subject') do |field|
-      field.solr_parameters = { :'spellcheck.dictionary' => 'subject' }
-      field.qt = 'search'
-      field.solr_local_parameters = { 
-        :qf => '$subject_qf',
-        :pf => '$subject_pf'
-      }
-    end
-
-    # "sort results by" select (pulldown)
-    # label in pulldown is followed by the name of the SOLR field to sort by and
-    # whether the sort is ascending or descending (it must be asc or desc
-    # except in the relevancy case).
-    config.add_sort_field 'score desc, pub_date_sort desc, title_sort asc', :label => 'relevance'
-    config.add_sort_field 'pub_date_sort desc, title_sort asc', :label => 'year'
-    config.add_sort_field 'author_sort asc, title_sort asc', :label => 'author'
-    config.add_sort_field 'title_sort asc, pub_date_sort desc', :label => 'title'
-
-    # If there are more than this many search results, no spelling ("did you 
-    # mean") suggestion is offered.
-    config.spell_max = 5
+    config.add_sort_field 'relevance', sort: 'score desc, sort_title_ssi asc', label: 'Relevance'
+    config.add_sort_field 'title', sort: 'sort_title_ssi asc', label: 'Title'
+    config.add_sort_field 'type', sort: 'sort_type_ssi asc', label: 'Type'
+    config.add_sort_field 'date', sort: 'sort_date_dtsi asc', label: 'Date (old to new)'
+    config.add_sort_field 'source', sort: 'sort_source_ssi asc', label: 'Source'
+    config.add_sort_field 'identifier', sort: 'id asc', label: 'Identifier'
   end
+
+
 
 end 
